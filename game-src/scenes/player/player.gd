d75b847d8251d197399452405e20a22281dcaf7e
Dewald Els <dewaldifels@gmail.com>
2024-11-03 20:44:10 +0100

    feat: Velocity component

--- game-src/scenes/player/player.gd
+++ game-src/scenes/player/player.gd
@@ -1,11 +1,9 @@
 class_name Player extends CharacterBody2D
 
 
-@export var move_speed: float
-@export var acceleration: float = 3.0
-@export var decceleration: float = 1.5
+var base_speed: float = 0.0
 
-
+@onready var velocity_component: VelocityComponent = %VelocityComponent
 @onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite
 
 @export var player_death: PackedScene
@@ -14,22 +12,29 @@ var is_dead: bool = false
 
 func _ready() -> void:
 	SignalBus.connect("player_popped", on_player_popped)
+	base_speed = velocity_component.max_speed
 	
 
 func _physics_process(_delta: float) -> void:
 	var movement_vector: Vector2 = get_input_vector()
+	var direction: Vector2 = movement_vector.normalized()
 	
-	if movement_vector == Vector2.ZERO:
-		velocity.x = move_toward(velocity.x, 0.0, decceleration)
-		velocity.y = move_toward(velocity.y, 0.0, decceleration)
-	else:
-		velocity.x = move_toward(velocity.x, movement_vector.x * move_speed, acceleration)
-		velocity.y = move_toward(velocity.y, movement_vector.y * move_speed, acceleration)
-		
-	move_and_slide()
-	play_animation()
-	face_movement_direction(movement_vector.x)
-	check_out_of_bounds()
+	velocity_component.accelerate(direction)
+	velocity_component.move(self)
+	
+	#var movement_vector: Vector2 = get_input_vector()
+	#
+	#if movement_vector == Vector2.ZERO:
+		#velocity.x = move_toward(velocity.x, 0.0, decceleration)
+		#velocity.y = move_toward(velocity.y, 0.0, decceleration)
+	#else:
+		#velocity.x = move_toward(velocity.x, movement_vector.x * move_speed, acceleration)
+		#velocity.y = move_toward(velocity.y, movement_vector.y * move_speed, acceleration)
+		#
+	#move_and_slide()
+	#play_animation()
+	#face_movement_direction(movement_vector.x)
+	#check_out_of_bounds()
 	
 	
 func get_input_vector() -> Vector2:
